c420f23cab5649bf136a74e61dd26c9b447c6a23
celiltat2361 <celilbtc@gmail.com>
2021-02-16 23:59:02 +0100

    Create a home component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Home from './pages/Home'
+
 function App() {
   return (
     <div className="App">
     <h1>Hej</h1>
+    <Home />
+
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import style from '../css/Home.module.css';
+function Home () {
+  return (
+    <div className={style.home}>
+      <div className={style.wrapper}>
+        <h1>here is calculator area</h1> 
+      </div>
+    </div>
+  )
+}
+
+export default Home;
