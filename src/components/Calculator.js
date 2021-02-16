74ed5407ab34586f4e08067e5ea17d6dc24ca841
celiltat2361 <celilbtc@gmail.com>
2021-02-17 00:28:17 +0100

    added calculated component

---
+++ src/components/Calculator.js
@@ -0,0 +1,11 @@
+import { calculator } from '../css/cClculator.module.css';
+
+function Calculator () {
+  return (
+    <div className={calculator}>
+      
+    </div>
+  )
+}
+
+export default Calculator;
